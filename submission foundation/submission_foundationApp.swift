fb433814c923e76c69d2d51806e827683a715783
firman-ali <firmanali22@gmail.com>
2023-04-06 09:48:45 +0700

    Initial Commit

---
+++ submission foundation/submission_foundationApp.swift
@@ -0,0 +1,17 @@
+//
+//  submission_foundationApp.swift
+//  submission foundation
+//
+//  Created by Firman Ali on 06/04/23.
+//
+
+import SwiftUI
+
+@main
+struct submission_foundationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
